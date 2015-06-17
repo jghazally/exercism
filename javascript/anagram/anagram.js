8f5404809c06808d410acae7c2a31115ca958a85
Jeffry Ghazally <jghazally@screamingcodemonkey.com>
2015-06-17 21:00:28 +1200

    Add in python HW

---
+++ javascript/anagram/anagram.js
@@ -0,0 +1,19 @@
+var Anagram = function(word) {
+	this.word = word;
+	return 
+}
+
+Anagram.prototype.matches = function(list) {
+	/*
+	for ( var i = 0; i < list.length; i++ ) {
+		// If the word char count does not match 
+		// the word char count it is no worth pursuing
+		if ( list[i].length !== this.word.length ) {
+			continue;
+		}
+
+	}
+	*/
+};
+
+module.export = Anagram;
