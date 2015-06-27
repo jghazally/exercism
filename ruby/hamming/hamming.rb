edebe6993e4d523f7517e44dd1c2d924a3b9d581
Jeffry Ghazally <jghazally@screamingcodemonkey.com>
2015-06-28 10:56:25 +1200

    Ruby hates: When you access two variables asymetrically. http://exercism.io/submissions/f56fc6155384497f8d5e7e2072982678

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,12 +1,12 @@
 class Hamming
 
-  VERSION = 3
+  VERSION = 4
 
   def self.compute(strand_a, strand_b)
 
     raise ArgumentError unless strand_a.length == strand_b.length
 
-    strand_a.each_char.with_index.count { |i, index| ( i != strand_b[index] ) }
+    strand_a.each_char.with_index.count { |_, i| ( strand_a[i] != strand_b[i] ) }
   end
 
 end
