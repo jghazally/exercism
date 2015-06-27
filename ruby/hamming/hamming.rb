e170b734b9fb178abbb932e72573709e6e993a10
Jeffry Ghazally <jghazally@screamingcodemonkey.com>
2015-06-28 09:58:12 +1200

    Fix up indenting

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,21 +1,21 @@
 class Hamming
 
-	VERSION = 1
+  VERSION = 1
 
-	def self.compute(strandA, strandB)
-		hamCount = 0
+  def self.compute(strandA, strandB)
+    hamCount = 0
 
-		if ( strandA.length != strandB.length ) 
-			raise(ArgumentError)
-		end
+    if ( strandA.length != strandB.length ) 
+      raise(ArgumentError)
+    end
 
-		strandB = strandB.split("")
-		strandA.split("").each_with_index do |i, index|
-			if ( i != strandB[index] )
-				hamCount += 1
-			end
-		end
-		return hamCount
-	end
+    strandB = strandB.split("")
+    strandA.split("").each_with_index do |i, index|
+      if ( i != strandB[index] )
+        hamCount += 1
+      end
+    end
+    return hamCount
+  end
 
 end
