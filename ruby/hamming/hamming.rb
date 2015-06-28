923462f7761341b558ba52c624a6dd5980180f04
Jeffry Ghazally <jghazally@screamingcodemonkey.com>
2015-06-28 15:40:04 +1200

    Fix rubocop errors

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,12 +1,10 @@
+# Hamming check the difference between 2 strands
 class Hamming
-
   VERSION = 4
 
   def self.compute(strand_a, strand_b)
+    fail ArgumentError unless strand_a.length == strand_b.length
 
-    raise ArgumentError unless strand_a.length == strand_b.length
-
-    strand_a.each_char.with_index.count { |_, i| ( strand_a[i] != strand_b[i] ) }
+    strand_a.each_char.with_index.count { |_, i| strand_a[i] != strand_b[i] }
   end
-
 end
