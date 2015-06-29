1b901d3e0ddd475777c3f2a15d565a9f37ce99dc
Jeffry Ghazally <jghazally@screamingcodemonkey.com>
2015-06-29 23:05:09 +1200

    Fix ham syntax JS

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,22 +1,22 @@
-var Ham = function(){};
+var Ham = {
+	compute: function(strandA, strandB) {
+		// The ham number
+		var hamCount = 0;
 
-Ham.compute = function(strandA, strandB) {
-	// The ham number
-	var hamCount = 0;
-
-	// If strands do not equal in length we cannot compute
-	if ( strandA.length != strandB.length ) {
-		throw "DNA strands must be of equal length.";
-	}
-
-	// Loop through one string and check them both at i
-	for ( var i = 0; i < strandA.length; i++ ) {
-		if ( strandA.charAt(i) != strandB.charAt(i) ) {
-			hamCount++;
+		// If strands do not equal in length we cannot compute
+		if ( strandA.length != strandB.length ) {
+			throw "DNA strands must be of equal length.";
 		}
-	}
 
-	return hamCount;
+		// Loop through one string and check them both at i
+		for ( var i = 0; i < strandA.length; i++ ) {
+			if ( strandA.charAt(i) != strandB.charAt(i) ) {
+				hamCount++;
+			}
+		}
+
+		return hamCount;
+	}
 };
 
 module.exports = Ham;
