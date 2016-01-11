3b7f6a7e50780ae33a96fb1c6f77fa198b371f8a
Jeffry Ghazally <jghazally@screamingcodemonkey.com>
2016-01-12 10:54:47 +1300

    Add beer-song v1

---
+++ javascript/beer-song/beer-song.js
@@ -0,0 +1,48 @@
+var BeerSong = function() {
+
+	this.verse = function(verse) {
+		var output = '';
+		var numBottles = verse + ' bottles';
+		var numRemainingBottles = (verse -1) + ' bottles';
+		var remainingBottles = 'Take one down and pass it around, ' + (verse - 1) + ' bottles';
+
+		if ( verse === 1 ) {
+			numBottles = '1 bottle';
+			remainingBottles = 'Take it down and pass it around, no more bottles';
+		}
+
+		if ( verse === 2 ) {
+			remainingBottles = 'Take one down and pass it around, 1 bottle';
+		}
+
+		if ( verse === 0 ) {
+			numBottles = 'No more bottles';
+			remainingBottles = 'Go to the store and buy some more, 99 bottles';
+		}
+
+		output += numBottles + ' of beer on the wall, ' + numBottles.toLowerCase() + ' of beer.\n' + remainingBottles + ' of beer on the wall.\n';
+
+		return output;
+	};
+
+	this.sing = function(start, end) {
+		var output = '';
+
+		if ( typeof end === 'undefined' ) {
+			end = -1;
+		} else {
+			end--;
+		}
+
+		for ( var i = start; i > end; i--  ) {
+			output += this.verse(i);
+			if ( (i - 1) > end ) {
+				output += '\n';
+			}
+		}
+
+		return output;
+	};
+};
+
+module.exports = BeerSong;
